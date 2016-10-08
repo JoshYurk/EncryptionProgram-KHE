5b2dd1f759f7ad2741337a75c17e2fb9c2a30f3d
Joshua Yurkovich <jyurkovi@kent.edu>
2016-10-07 22:08:47 -0400

    Added logic.cpp and added some of the methods and variables to the header file

--- EncryptionProgram/EncryptionProgram/crypt.h
+++ EncryptionProgram/EncryptionProgram/crypt.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef ENCRYPT_DECRYPT
+#define ENCRYPT_DECRYPT
+
+//Includes
+#include <string>
+#include <iostream>
+
+//Usings
+using std::cout;
+using std::endl;
+using std::string;
+
+
+//Functions
+string encrypt(string unencrypted);
+string decrypt(string encrypted);
+int convertToBinary(string input);
+int convertToASCII(int input);
+int keyConversion(string key);
+
+//Variables
+string userInput;
+string encryptedString;
+string decryptedString;
+
+#endif 
+
